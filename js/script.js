e8af15604d2c10bacdc08245863960a91a0af777
MichelML <michmoreau.l@gmail.com>
2016-07-14 13:10:42 -0400

    adding nyt api function

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ function loadData() {
             articlesHTML = "",
             count = 1;
         $.each(articles, function(key,article) {
-            articlesHTML+="<a href='" + article.web_url+"'><li class='nyt-article' id='article'"+count+">" + 
+            articlesHTML+="<a href='" + article.web_url+"' target='_blank'><li class='nyt-article' id='article'"+count+">" + 
                           "<h3 class='nyt-title'>" + article.headline.main + "<small> - " + article.pub_date.substr(0,10)  + "</small></h3>" + 
                           "<p class='nyt-lead-paragraph'>" + article.lead_paragraph+ "</p></li></a>"; 
             count++;
