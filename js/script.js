9f0ad18422d15f6d4a6825336a20dcaa326420f2
MichelML <michmoreau.l@gmail.com>
2016-07-14 16:35:20 -0400

    adding nyt api function

--- js/script.js
+++ js/script.js
@@ -14,39 +14,45 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-    var streetStr = ($street.val() !== "") ? $street.val().replace(/\s/g,"-") + ',' : "";
+    var streetStr = ($street.val() !== "") ? $street.val().replace(/\s/g, "-") + ',' : "";
     var cityStr = ($city.val() !== "") ? $city.val().replace(/\s/g, "-") : "";
     var apiKey = "&key=AIzaSyD9meXfYM0jaOy28AC87HT0p8SR5f8RTjg";
-    var srcStr = (streetStr+cityStr) ? "https://maps.googleapis.com/maps/api/streetview?size=600x300&location="+streetStr+cityStr+apiKey
-                                        : $bgimg.attr("src");
+    var srcStr = (streetStr + cityStr) ? "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + streetStr + cityStr + apiKey : $bgimg.attr("src");
     console.log(srcStr);
     $bgimg.attr("src", srcStr);
     // Load NYT articles
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
-          'api-key': "587c93bf09b14a70b069b5352a646e29",
-          'q': cityStr,
-          'sort': "newest",
-          'page': 0
+        'api-key': "587c93bf09b14a70b069b5352a646e29",
+        'q': cityStr,
+        'sort': "newest",
+        'page': 0
     });
     console.log(url);
-    $.getJSON(url,function(data) {
+    $.getJSON(url, function(data) {
         var articles = data.response.docs,
             articlesHTML = "",
             count = 1;
-        $.each(articles, function(key,article) {
+        if (articles.length === 0) articlesHTML += "<h3>We are sorry. There was no article related to your query.</h3>";
+        else {
+        $.each(articles, function(key, article) {
             if (article.lead_paragraph === null) article.lead_paragraph = "Click here to read the article";
-            if (article.lead_paragraph.length > 500) article.lead_paragraph = article.lead_paragraph.substr(0,500);
-            articlesHTML+="<a href='" + article.web_url+"' target='_blank'><li class='nyt-article' id='article'"+count+">" + 
-                          "<h3 class='nyt-title'>" + article.headline.main + "<small> - " + article.pub_date.substr(0,10)  + "</small></h3>" + 
-                          "<p class='nyt-lead-paragraph'>" + article.lead_paragraph+ " [...]</p></li></a>"; 
+            if (article.lead_paragraph.length > 500) article.lead_paragraph = article.lead_paragraph.substr(0, 500);
+            articlesHTML += "<a href='" + article.web_url + "' target='_blank'><li class='nyt-article' id='article'" + count + ">" +
+                "<h3 class='nyt-title'>" + article.headline.main + "<small> - " + article.pub_date.substr(0, 10) + "</small></h3>" +
+                "<p class='nyt-lead-paragraph'>" + article.lead_paragraph + " [...]</p></li></a>";
             count++;
         });
+        }
         $nytElem.html(articlesHTML);
+        
+    })
+    .error(function () {
+        var defaultAnswer = "<h3>New York Times Articles could Not Be Loaded.";
+        $nytElem.html(defaultAnswer);
     });
-    // YOUR CODE GOES HERE!
-
+    
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
